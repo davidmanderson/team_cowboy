34f9291f77d85deadbe684db20e4632a84fd06df
David Anderson <davidmartinanderson@gmail.com>
2018-02-06 22:41:40 -0800

    handle optional values

--- lib/team_cowboy/client/event.rb
+++ lib/team_cowboy/client/event.rb
@@ -22,16 +22,16 @@ module TeamCowboy
       get("Event_GetAttendanceList", params)
     end
 
-    def save_event_rsvp(team_id:, event_id:, status:, add_male: nil, add_femail: nil, comments: nil)
+    def save_event_rsvp(team_id:, event_id:, status:, extra_male: nil, extra_female: nil, comments: nil)
       params = { 
         userToken:  self.user_token,
         teamId:     team_id,
         eventId:    event_id,
         status:     status, # yes|maybe|available|no|noresponse
-        addMale:    add_male, # integer
-        addFemale:  add_female, # integer
-        comments:   comments
       }
+      params[:'addlMale'] = extra_male if extra_male       # integer
+      params[:'addlFemale'] = extra_female if extra_female # integer
+      params[:'comments'] = comments if comments
 
       post("Event_SaveRSVP", params)
     end
