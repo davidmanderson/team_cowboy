3bff7327c0878af6b5f23c4e9a6a313b418c6348
David Anderson <davidmartinanderson@gmail.com>
2018-05-17 23:25:52 -0700

    adding roster

--- lib/team_cowboy/client/team.rb
+++ lib/team_cowboy/client/team.rb
@@ -22,6 +22,18 @@ module TeamCowboy
 
       get("Team_GetMessages", params)
     end
+
+    def roster(team_id:, user_id: nil, include_inactive:nil, sort_by: nil, sort_direction: nil)
+      params = {
+        userToken:      self.user_token,
+        teamId:         team_id,
+        userId:         user_id,
+        sort_by:        sort_by, # firstName|playerType|sex
+        sort_direction: sort_direction, # ASC|DESC
+      }
+
+      get("Team_GetRoster", params)
+    end
           
   end
 end
