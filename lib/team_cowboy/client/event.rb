d6cf16fe7fc250ca98d38b39f6ee34c07d910b8e
David Anderson <davidmartinanderson@gmail.com>
2019-07-21 14:36:10 -0700

    allow setting rsvps of other users on event

--- lib/team_cowboy/client/event.rb
+++ lib/team_cowboy/client/event.rb
@@ -22,16 +22,18 @@ module TeamCowboy
       get("Event_GetAttendanceList", params)
     end
 
-    def save_event_rsvp(team_id:, event_id:, status:, extra_male: nil, extra_female: nil, comments: nil)
+    def save_event_rsvp(team_id:, event_id:, status:, extra_male: nil, extra_female: nil, comments: nil, user_id: nil)
       params = { 
-        userToken:  self.user_token,
-        teamId:     team_id,
-        eventId:    event_id,
-        status:     status, # yes|maybe|available|no|noresponse
+        userToken:    self.user_token,
+        teamId:       team_id,
+        eventId:      event_id,
+        status:       status, # yes|maybe|available|no|noresponse
+
       }
       params[:'addlMale'] = extra_male if extra_male       # integer
       params[:'addlFemale'] = extra_female if extra_female # integer
       params[:'comments'] = comments if comments
+      params[:'rsvpAsUserId'] = user_id if user_id
 
       post("Event_SaveRSVP", params)
     end
